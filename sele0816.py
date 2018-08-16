9269b745eb57d233427a556cfb8a8ecc438e55a5
herlla <hfhu910927@163.com>
2018-08-16 16:26:27 +0800

    v1
    selenium test

---
+++ sele0816.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 16 15:14:19 2018
+
+@author: huhuifang
+"""
+
+from selenium import webdriver
+driver=webdriver.Firefox()
+driver.get('https://www.baidu.com')
